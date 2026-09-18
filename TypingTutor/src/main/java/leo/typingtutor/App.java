67c57c5d3970a6d117817109086aa5099c3b49f8
Leo <6375071@edu.vaniercollege.qc.ca>
2026-09-18 15:31:14 -0400

    Add event to display the key typed by user

--- TypingTutor/src/main/java/leo/typingtutor/App.java
+++ TypingTutor/src/main/java/leo/typingtutor/App.java
@@ -3,7 +3,9 @@ package leo.typingtutor;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -21,6 +23,8 @@ public class App extends Application {
         HBox r1 = new HBox(10);
         HBox r2 = new HBox(10);
         HBox r3 = new HBox(10);
+        
+        Label keyTyped = new Label();
         
         String[] r1Keys = {"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"};
         String[] r2Keys = {"A", "S", "D", "F", "G", "H", "J", "K", "L"};
@@ -43,15 +47,22 @@ public class App extends Application {
         
         TextField expectedText = new TextField("This is the text that is "
                 + "expected to be typed");
-
-
         TextField typedResponse = new TextField();
         
-        vb.getChildren().addAll(expectedText, typedResponse, r1, r2, r3);
+        vb.getChildren().addAll(expectedText, typedResponse, keyTyped, r1, r2, r3);
         
         Scene scene = new Scene(vb, 600, 400);
         stage.setScene(scene);
         stage.show();
+        
+        // Displays the key typed
+        scene.setOnKeyPressed(event -> {
+            KeyCode keyCode = event.getCode();
+            keyTyped.setText("" + keyCode);
+            
+            stage.setScene(scene);
+            stage.show();
+        });
     }
 
     public static void main(String[] args) {
